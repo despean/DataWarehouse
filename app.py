ce66dce7f6a6b761719792730b094fc4fbe806d7
despean <despean15@gmail.com>
2020-11-24 02:32:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 model = pickle.load(open('best_classifier.pkl', 'rb'))
 
 
